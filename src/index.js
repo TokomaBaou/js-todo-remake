6b01aa3787ff9ca76cd96ef551c7837d09ab9bb4
MAKOTO OBA <86038701+TokomaBaou@users.noreply.github.com>
2021-09-18 23:57:16 +0900

    [add]JS javaScriptの追加

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  //テキストボックスの値を取得し、初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  //li生成
+  const li = document.createElement("li");
+  li.className = "list";
+
+  //div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  //p生成
+  const p = document.createElement("p");
+  p.innerText = inputText;
+
+  //liタグの子要素に各要素を設定
+  li.appendChild(div);
+  //divタグに追加
+  div.appendChild(p);
+
+  //未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(li);
+};
+
+//クリックイベントを付与
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
