ba054c1c840fc1e54d40d31336e792523aa7541f
MAKOTO OBA <86038701+TokomaBaou@users.noreply.github.com>
2021-09-24 22:58:55 +0900

    [add]delete 削除機能の共通化

--- src/index.js
+++ src/index.js
@@ -7,12 +7,12 @@ const onClickAdd = () => {
 
   createIncompleteList(inputText);
 };
-//共通化したい
-//未完了リストから指定の要素を削除
-// const deleteFromIncompleteList = (target) => {
-//   //完了ボタンを押したら未完了リストから削除する
-//   document.getElementById("incomplete-list").removeChild(target);
-// };
+// 共通化したい
+// 未完了リストから指定の要素を削除
+const deleteFromIncompleteList = (target) => {
+  //完了ボタンを押したら未完了リストから削除する
+  document.getElementById("incomplete-list").removeChild(target);
+};
 
 //未完了リストに追加する関数
 const createIncompleteList = (text) => {
